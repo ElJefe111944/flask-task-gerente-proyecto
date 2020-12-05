2931449b00d738f5ba20f81f81b3e3df3762b331
ElJefe111944 <jamesfarr1@sky.com>
2020-12-05 10:49:48 +0000

    Add Manage Categories template

--- app.py
+++ app.py
@@ -145,6 +145,13 @@ def delete_task(task_id):
 
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
+
 
 
 if __name__ == "__main__":
